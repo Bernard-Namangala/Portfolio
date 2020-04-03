a6f9f91b4b288ef9e8e38a1cd24dda521c6953ea
Bernard Namangala <bernardnamangala@gmail.com>
2020-04-03 19:44:32 +0200

    commit

--- Portfolio/settings/production.py
+++ Portfolio/settings/production.py
@@ -2,14 +2,14 @@ import django_heroku
 from .base import *
 DEBUG = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ('SECRET_KEY')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Portfolio',
         'USER': 'postgres',
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'PASSWORD': os.environ('DATABASE_PASSWORD'),
         'HOST': 'localhost'
     }
 }
