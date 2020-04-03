f531fc884bb8e32ae3433e76ddc8ba660e4120ae
Bernard Namangala <bernardnamangala@gmail.com>
2020-04-03 18:47:13 +0200

    commit

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -4,11 +4,6 @@ from django.core.exceptions import ImproperlyConfigured
 import django_heroku
 
 
-
-
-
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
