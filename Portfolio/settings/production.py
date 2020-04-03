bb3249748500cda6b0f28b7297b87f47161a107e
Bernard Namangala <bernardnamangala@gmail.com>
2020-04-03 19:58:13 +0200

    commit

--- Portfolio/settings/production.py
+++ Portfolio/settings/production.py
@@ -6,14 +6,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/static'),
 ]
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Portfolio',
         'USER': 'postgres',
-        'PASSWORD': os.environ('DATABASE_PASSWORD'),
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
         'HOST': 'localhost'
     }
 }
