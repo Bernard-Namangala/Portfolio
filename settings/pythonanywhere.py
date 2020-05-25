e5eb310a1159917063789e63fbbadd14de5cfcbb
Bernard Namangala <bernardnamangala@gmail.com>
2020-05-25 10:44:03 +0200

    edited pythonanywhere settings

--- settings/pythonanywhere.py
+++ settings/pythonanywhere.py
@@ -1,22 +1,7 @@
 from .base import *
 DEBUG = True
 
-with open("secrets.json") as f:
-    secrets = json.loads(f.read())
-
-
-def get_secret(setting, the_secrets=secrets):
-    """
-    get the setting or return explicit exception
-    """
-    try:
-        return the_secrets[setting]
-    except KeyError:
-        error_message = "Set {0} environmental variable".format(setting)
-        raise ImproperlyConfigured(error_message)
-
-
-SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = "@u)^5#hm-6gc=(x5408r$qfyhwq$xo+9)yw=^eq52%rlj^&6mx"
 
 
 DATABASES = {
@@ -24,7 +9,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Portfolio',
         'USER': 'postgres',
-        'PASSWORD': get_secret("DATABASE_PASSWORD"),
+        'PASSWORD': "ilovecoding1@",
         'HOST': 'localhost'
     }
 }
